5b026e41cbd8ac45301705dec5d342c9cd17eed5
rahul <rahul111568@gmail.com>
2023-03-13 13:24:11 +0530

    initial

---
+++ server/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import utils
+
+app = Flask(__name__)
+
+
+@app.route('/classification', methods=['GET', 'POST'])
+def classifyIMG():
+    IMGdata = request.form.get('IMGdata')
+    response = jsonify(utils.classify_image(IMGdata))
+
+    response.headers.add('Acess-Control-Allow-Origin', '*')
+    return response
+
+
+if __name__ == "__main__":
+    print('Starting Python flask server for IMGclassification')
+    utils.load_saved_artifacts()
+    app.run()
